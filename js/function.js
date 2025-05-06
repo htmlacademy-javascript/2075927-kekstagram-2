bd292500a4c80f8c5c80c0417ee118792fc8ad1e
one-donovan <odonovanone@gmail.com>
2025-05-06 17:32:01 +0400

    домашнее задание 2

---
+++ js/function.js
@@ -0,0 +1,26 @@
+function checkStringLength(string, length) {
+  if (string.length <= length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+checkStringLength();
+
+const checkLength = (string, length) => string.length <= length;
+
+checkLength('ffff', 33);
+
+
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  let emptyString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    emptyString += normalizedString[i];
+  }
+  return emptyString === normalizedString;
+};
+
+
+isPalindrome();
